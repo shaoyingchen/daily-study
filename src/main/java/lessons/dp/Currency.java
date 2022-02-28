973e81d9a0d19c91a3f0992068eb94d255743d96
shaoying <614886829@qq.com>
2022-02-28 15:23:06 +0800

    货币转化为字符串问题

---
+++ src/main/java/lessons/dp/Currency.java
@@ -0,0 +1,50 @@
+package lessons.dp;
+
+/**
+ * arr是货币数组，其中的值都是正数。再给定一个正数aim。
+ * 每个值都认为是一张货币，
+ * 即便是值相同的货币也认为每一张都是不同的，
+ * 返回组成aim的方法数
+ * 例如：arr = {1,1,1}，aim = 2
+ * 第0个和第1个能组成2，第1个和第2个能组成2，第0个和第2个能组成2
+ * 一共就3种方法，所以返回3
+ *
+ * @author chensy6
+ * @CreateDate 2022/2/28 14:52
+ **/
+public class Currency {
+
+    public static int currency(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        return process(arr, 0, aim);
+    }
+
+    public static int process(int[] arr, int index, int rest) {
+        if (rest < 0) {
+            return 0;
+        }
+        if (index == arr.length) {
+            return rest == 0 ? 1 : 0;
+        }
+        return process(arr, index + 1, rest) + process(arr, index + 1, rest - arr[index]);
+    }
+
+    public static int currency2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+
+        int N = arr.length;
+        int[][] dp = new int[N + 1][aim + 1];
+        dp[N][0] = 1;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                dp[index][rest] = dp[index + 1][rest] + (rest - arr[index] < 0 ? 0 : dp[index + 1][rest - arr[index]]);
+            }
+        }
+        return dp[0][aim];
+    }
+
+}
